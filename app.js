a09b8f0d68e861d184a63dbf88518f8b7111dc95
Gabryel Velli <gabryel.velli@gmail.com>
2025-05-21 10:24:44 -0300

    Atualizacao para subir projeto

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const initDb = require('./config/initDb');
 const db = require('./config/db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
